3f86799c9bbe24e5303465de9a964350660dbb23
aditya <dev@chimeracoder.net>
2014-06-08 16:44:07 -0400

    Add basic example of 'select' with channels

--- main.go
+++ main.go
@@ -3,26 +3,35 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"time"
 )
 
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
 //START OMIT
 func Greet(name string, response_chan chan string) {
-	time.Sleep(3 * time.Second)
+	secs := rand.Intn(10)
+	log.Printf("Sleeping %d seconds before greeting %s", secs, name)
+	time.Sleep(time.Duration(secs) * time.Second)
 	greeting := fmt.Sprintf("Greetings, %s!", name)
 	response_chan <- greeting
-	log.Print("Greeting function is tired - sleeping for a bit")
-	time.Sleep(3 * time.Second)
-	log.Print("now the greeting function is done sleeping - terminating")
 }
 
 func main() {
 
-	cs := make(chan string)
-	go Greet("Alice", cs)
-	log.Print("Continuing execution while we wait for greeter to respond")
-	greeting := <-cs
-	log.Print(greeting)
-	time.Sleep(10 * time.Second)
+	ac := make(chan string)
+	bc := make(chan string)
+	go Greet("Alice", ac)
+	go Greet("Bob", bc)
+	select {
+	case greeting := <-ac:
+		log.Print(greeting)
+	case greeting := <-bc:
+		log.Print(greeting)
+	}
 }
+
 //END OMIT
